4baf2846234b719f1de8c5f6a6da412eabfdf48a
annaszutkowska <ankaszutek@gmail.com>
2024-05-06 21:55:04 +0200

    Add data visualization Datasets' sizes comparison Image samples display Distribution of classes in training dataset

---
+++ config.py
@@ -0,0 +1,7 @@
+LABELS = ['PNEUMONIA', 'NORMAL']
+IMG_SIZE = 150
+
+DATA_DIR = './data'
+TRAIN_DIR = f'{DATA_DIR}/train'
+TEST_DIR = f'{DATA_DIR}/test'
+VAL_DIR = f'{DATA_DIR}/val'
--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 if __name__ == "__main__":
-    # Define the path to the test data and the model
-    test_data_path = './data/'
-    model_path = './models/captcha_solver.h5 TBD'
-
     # Load the model
 
     # Load and preprocess the test data
