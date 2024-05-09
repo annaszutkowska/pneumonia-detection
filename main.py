5efce422472232c775b3d7e732bec4b8adf539b9
annaszutkowska <ankaszutek@gmail.com>
2024-05-09 23:19:50 +0200

    Correct data imbalance

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ def get_training_data(data_dir: str) -> np.ndarray:
     return np.array(data, dtype=object)
 
 
+def shuffle_data(*args: np.ndarray) -> np.ndarray:
+    combined_data = np.concatenate(args, axis=0)
+    np.random.shuffle(combined_data)
+    return combined_data
+
+
 if __name__ == "__main__":
     print("Loading the data")
     train, val, test = (get_training_data(DIR) for DIR in [TRAIN_DIR, VAL_DIR, TEST_DIR])
@@ -33,6 +39,19 @@ if __name__ == "__main__":
     show_sample_images(train, CLASS_LABELS)
 
     print("Correcting data imbalance")
+    combined_dataset = shuffle_data(train, val, test)
+    num_train = int(0.8 * len(combined_dataset))
+    num_val = int(0.1 * len(combined_dataset))
+    num_test = len(combined_dataset) - num_train - num_val
+    train = combined_dataset[:num_train]
+    val = combined_dataset[num_train:num_train + num_val]
+    test = combined_dataset[num_train + num_val:]
+    print(f"New dataset sizes: train - {len(train)}, val - {len(val)}, test - {len(test)}")
+
+    print("Visualizing the dataset after correcting the imbalance")
+    dataset_sizes = [len(train), len(val), len(test)]
+    plot_dataset_proportions(dataset_sizes, DATASET_LABELS)
+    plot_class_distribution(train)
 
     # Load the model
 
--- src/data_visualization.py
+++ src/data_visualization.py
@@ -7,22 +7,18 @@ from matplotlib import pyplot as plt
 def plot_dataset_proportions(dataset_sizes: List[int], dataset_labels: List[str]) -> None:
     total = sum(dataset_sizes)
     proportions = [size / total * 100 for size in dataset_sizes]
-
     plt.figure(figsize=(8, 4))
     ax = sns.barplot(x=dataset_labels, y=proportions, hue=dataset_labels, palette='pastel', dodge=False)
     plt.title('Dataset Proportions')
     plt.xlabel('Dataset')
     plt.ylabel('Percentage of Total (%)')
-
     for i, prop in enumerate(proportions):
         ax.text(i, prop, f'{prop:.2f}%', ha='center', va='bottom', fontweight='bold', color='teal')
-
     plt.show()
 
 
-def plot_class_distribution(train_dataset: List[List[int]]) -> None:
+def plot_class_distribution(train_dataset: np.ndarray) -> None:
     labels_list = ["Pneumonia" if i[1] == 0 else "Normal" for i in train_dataset]
-
     sns.set_style('whitegrid')
     plt.figure(figsize=(10, 6))
     sns.histplot(labels_list, discrete=True, color='lightpink', edgecolor='black', linewidth=1.5)
@@ -34,14 +30,12 @@ def plot_class_distribution(train_dataset: List[List[int]]) -> None:
     plt.show()
 
 
-def show_sample_images(train_dataset: List[List[int]], labels: List[str]):
+def show_sample_images(train_dataset: np.ndarray, labels: List[str]):
     fig, axes = plt.subplots(1, 2, figsize=(10, 5))
     axes[0].imshow(train_dataset[0][0], cmap='gray')
     axes[0].set_title(f'First Image: {labels[train_dataset[0][1]]}', color='palevioletred')
     axes[0].axis('off')
-
     axes[1].imshow(train_dataset[-1][0], cmap='gray')
     axes[1].set_title(f'Last Image: {labels[train_dataset[-1][1]]}', color='teal')
     axes[1].axis('off')
-
     plt.show()
